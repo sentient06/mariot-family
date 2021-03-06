b399599fb2c6f60068da021bc817116b67cac2a0
Giancarlo Mariot <gian@kame.home>
2021-03-06 15:57:21 +0000

    Added photo gallery, some footnotes and a couple of paragraphs to Matteo and Teresa's pages.

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,8 +1,8 @@
-var lightboxWrappers = document.getElementsByClassName('lightbox');
 var htmlBody = document.getElementsByTagName('body').item(0);
-
 var lightboxBack = document.createElement("div");
 var lightboxImageWrapper = document.createElement("div");
+var lightboxLabel = document.createElement("div");
+var lightboxClose = document.createElement("a");
 var lightboxImage = document.createElement("img");
 
 htmlBody.append(lightboxBack);
@@ -11,12 +11,26 @@ lightboxBack.className = "lightboxBack";
 lightboxBack.append(lightboxImageWrapper);
 
 lightboxImageWrapper.className = "lightboxImageWrapper";
+
+lightboxLabel.className = "lightboxLabel";
+lightboxClose.className = "lightboxClose";
+lightboxClose.innerHTML = "×";
+lightboxClose.setAttribute('href', '');
+
+lightboxImageWrapper.append(lightboxLabel);
 lightboxImageWrapper.append(lightboxImage);
+lightboxImageWrapper.append(lightboxClose);
+
+lightboxImage.style = "max-width: 100%;";
 
 lightboxBack.addEventListener("click", function(e) {
     lightboxBack.setAttribute('style', "display: none;");
     lightboxImage.setAttribute('src', '');
 });
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+var lightboxWrappers = document.getElementsByClassName('lightbox');
 
 for (var i = 0; i < lightboxWrappers.length; i++) {
     applyLightbox(lightboxWrappers.item(i));
@@ -31,13 +45,19 @@ function applyLightbox(wrapper) {
 
 function lightboxAnchor(anchor) {
     var imagePath = anchor.href;
+    var labelStr = anchor.title;
+    console.log(anchor);
     anchor.addEventListener("click", function(e) {
         e.preventDefault();
         var lightboxClosed = lightboxBack.style.display === "none";
         if (lightboxClosed) {
+            htmlBody.className = "lightboxOpen";
+            lightboxLabel.innerHTML = labelStr ?? "";
             lightboxImage.setAttribute('src', imagePath);
             lightboxBack.setAttribute('style', "display: auto;");
         } else {
+            htmlBody.className = "";
+            lightboxLabel.innerHTML = "";
             lightboxImage.setAttribute('src', '');
             lightboxBack.setAttribute('style', "display: none;");
         }
